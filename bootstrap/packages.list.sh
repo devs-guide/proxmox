585ee78bb6f612536faf96da36f3b17294d02f15
Climate AmanteDeLaVida <git.climate@amante.co>
2025-11-19 00:26:24 -0600

    init: packages.list.sh

---
+++ bootstrap/packages.list.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+## Base / Ansible prerequisite package list
+
+BASE_PACKAGES=(
+  sudo
+  vim
+  nano
+  curl
+  wget
+  git
+  rsync
+  htop
+  python3
+  python3-venv
+  python3-pip
+  lsb-release
+  ca-certificates
+  openssh-server
+  acl
+)
